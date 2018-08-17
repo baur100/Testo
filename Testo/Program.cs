da5a2a40bd3ed49655089fb32f204d7ec605e59d
Baurzhan Zhylkaidarov <Baurzhan.zh@hotmail.com>
2018-08-17 12:27:23 -0400

    Database First finished

--- Testo/Program.cs
+++ Testo/Program.cs
@@ -10,7 +10,17 @@ namespace Testo
     {
         static void Main(string[] args)
         {
-
+            var context = new EntiryStaffEntities();
+            var person = new Staff()
+            {
+                DateOfBirth = new DateTime(1975, 12, 20),
+                FirstName = "Ivan",
+                LastName = "Petrov",
+                Id = 2,
+                Position = "Manager"
+            };
+            context.Staffs.Add(person);
+            context.SaveChanges();
         }
     }
 }
